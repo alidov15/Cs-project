85397bf21c87521fb32bfc9c739a3874065e714c
user <user@ALICHOPLO>
2024-12-10 19:19:08 +0300

    Add project files.

---
+++ WindowsFormsApp7/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsApp7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonGuess = new System.Windows.Forms.Button();
+            this.buttonNewGame = new System.Windows.Forms.Button();
+            this.LabelResult = new System.Windows.Forms.Label();
+            this.textBoxGuess = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonGuess
+            // 
+            this.buttonGuess.Location = new System.Drawing.Point(260, 275);
+            this.buttonGuess.Name = "buttonGuess";
+            this.buttonGuess.Size = new System.Drawing.Size(75, 23);
+            this.buttonGuess.TabIndex = 0;
+            this.buttonGuess.Text = "Guess";
+            this.buttonGuess.UseVisualStyleBackColor = true;
+            this.buttonGuess.Click += new System.EventHandler(this.buttonGuess_Click);
+            // 
+            // buttonNewGame
+            // 
+            this.buttonNewGame.Location = new System.Drawing.Point(373, 275);
+            this.buttonNewGame.Name = "buttonNewGame";
+            this.buttonNewGame.Size = new System.Drawing.Size(75, 23);
+            this.buttonNewGame.TabIndex = 1;
+            this.buttonNewGame.Text = "New Game";
+            this.buttonNewGame.UseVisualStyleBackColor = true;
+            // 
+            // LabelResult
+            // 
+            this.LabelResult.AutoSize = true;
+            this.LabelResult.Location = new System.Drawing.Point(134, 161);
+            this.LabelResult.Name = "LabelResult";
+            this.LabelResult.Size = new System.Drawing.Size(455, 13);
+            this.LabelResult.TabIndex = 2;
+            this.LabelResult.Text = "I have a number between 1 and 1000--can you guess my number? Please enter your fi" +
+    "rst guess";
+            // 
+            // textBoxGuess
+            // 
+            this.textBoxGuess.Location = new System.Drawing.Point(302, 221);
+            this.textBoxGuess.Name = "textBoxGuess";
+            this.textBoxGuess.Size = new System.Drawing.Size(100, 20);
+            this.textBoxGuess.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxGuess);
+            this.Controls.Add(this.LabelResult);
+            this.Controls.Add(this.buttonNewGame);
+            this.Controls.Add(this.buttonGuess);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonGuess;
+        private System.Windows.Forms.Button buttonNewGame;
+        private System.Windows.Forms.Label LabelResult;
+        private System.Windows.Forms.TextBox textBoxGuess;
+    }
+}
+
---
+++ WindowsFormsApp7/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp7
+{
+    public partial class Form1 : Form
+    {
+        private int numberToGuess;
+        private Random random;
+
+        public Form1()
+        {
+            InitializeComponent();
+            random = new Random();
+            StartNewGame();
+        }
+
+        private void StartNewGame()
+        {
+            numberToGuess = random.Next(1, 1001);
+            this.BackColor = SystemColors.Control;
+            textBoxGuess.Enabled = true;
+            textBoxGuess.Text = "I have a number between 1 and 1000--can you guess my number? Please enter your first guess.";
+            textBoxGuess.Enabled = true;
+            textBoxGuess.Text = ""; // Clear previous input
+        }
+
+        private void buttonGuess_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBoxGuess.Text, out int guess))
+            {
+                if (guess < numberToGuess)
+                {
+                    textBoxGuess.Text = "Too Low";
+                    this.BackColor = Color.Blue;
+                }
+                else if (guess > numberToGuess)
+                {
+                    textBoxGuess.Text = "Too High";
+                    this.BackColor = Color.Blue;
+                }
+                else
+                {
+                    MessageBox.Show("Correct!", "Congratulations");
+                    this.BackColor = Color.Green;
+                    textBoxGuess.Enabled = false; // Disable the TextBox after correct guess
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid number.", "Invalid Input");
+            }
+        }
+
+        private void buttonNewGame_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+    }
+}
